a6070d409e49fc0b56e405f382c7a67e68993f88
Walid Hanif Ataullah <walidhnf12@gmail.com>
2023-06-16 22:19:54 +0700

    Menambahkan UI Absensi Menggunakan QR Code

---
+++ StudentInfo.cs
@@ -0,0 +1,72 @@
+﻿using DGVPrinterHelper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp17
+{
+    public partial class StudentInfo : Form
+    {
+        private MySqlConnection con = new MySqlConnection();
+        //private object stringformatflags;
+
+        public StudentInfo()
+        {
+            InitializeComponent();
+            con.ConnectionString = @"server=localhost;database=user_info;userid=root;password=;";
+        }
+
+        private void StudentInfo_Load(object sender, EventArgs e)
+        {
+            //Show Data on datagridview from database 
+            try
+            {
+                con.Open();
+                MySqlCommand coman = new MySqlCommand();
+                coman.Connection = con;
+                string query = "select * from attendance_tbl";
+                coman.CommandText = query;
+                MySqlDataAdapter da = new MySqlDataAdapter(coman);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DGVPrinter printer = new DGVPrinter();
+            printer.Title = "STUDENT ATTENDANCE SYSTEM REPROT FORM";
+            printer.SubTitleFormatFlags = StringFormatFlags.LineLimit | StringFormatFlags.NoClip;
+            printer.PageNumbers = true;
+            printer.PageNumberInHeader = false;
+            printer.PorportionalColumns = true;
+            printer.HeaderCellAlignment = StringAlignment.Near;
+            printer.Footer = "Footer";
+            printer.FooterSpacing = 15;
+            printer.printDocument.DefaultPageSettings.Landscape = true;
+            printer.PrintDataGridView(dataGridView1);
+            
+        }
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+        }
+        
+    }
+}
